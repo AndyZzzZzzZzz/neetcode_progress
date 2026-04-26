7834edb045d6bb7b8048fe9a51ac9309855f4f14
Andy Zhang <129815299+AndyZzzZzzZzz@users.noreply.github.com>
2026-04-26 18:18:29 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int arr[26] = {0};
+        for(char c : s) arr[c - 'a']++;
+        for(char c : t) arr[c - 'a']--;
+        for(int i : arr) if(i != 0) return false;
+        return true;
+            
+    }
+};
