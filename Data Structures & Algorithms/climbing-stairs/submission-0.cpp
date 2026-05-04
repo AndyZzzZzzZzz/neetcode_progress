1fbe4ee3776c253d39063f28571d9e4232068937
Andy Zhang <129815299+AndyZzzZzzZzz@users.noreply.github.com>
2026-05-04 13:56:46 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int curr = 0;
+        int prev1 = 1;
+        int prev2 = 0;
+        for(int i{1}; i <= n; ++i)
+        {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+
+    }
+};
