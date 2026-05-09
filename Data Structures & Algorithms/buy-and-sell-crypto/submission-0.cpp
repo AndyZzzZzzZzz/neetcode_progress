a5d0d86f235c35317fc34c1afc47bbf8210311af
Andy Zhang <129815299+AndyZzzZzzZzz@users.noreply.github.com>
2026-05-09 15:56:29 +0800

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        // greedy selection
+        int ans = 0;
+        int buy = prices[0];
+        for(int i{1}; i < prices.size(); ++i)
+        {
+            ans = max(ans, prices[i] - buy);
+            buy = min(buy, prices[i]);
+        }
+        return ans;
+    }
+};
