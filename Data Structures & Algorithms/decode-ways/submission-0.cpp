515ee58ebfd301743de2b5ee34b24a294a47c5c5
Andy Zhang <129815299+AndyZzzZzzZzz@users.noreply.github.com>
2026-06-16 18:26:28 +0800

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        int n = s.size();
+        // dp[i] represent ways to decode prefix i 
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 1;
+        if(s[0] != '0') dp[1] = 1;
+
+        for(int i{2}; i <= n; ++i) {
+            if(s[i-1] != '0') dp[i] += dp[i-1];
+            if((s[i-2] == '2' && s[i-1] <= '6') || (s[i-2] == '1')) dp[i] += dp[i-2];
+        }
+        return dp[n];
+
+    }
+};
